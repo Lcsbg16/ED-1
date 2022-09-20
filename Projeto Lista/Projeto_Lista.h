32a5500d6a9b3efa016d47eef2bca9af9cac02ca
Lucas Barbosa <81834902+Lcsbg16@users.noreply.github.com>
2022-09-20 15:12:14 -0300

    Add files via upload

---
+++ Projeto Lista/Projeto_Lista.h
@@ -0,0 +1,24 @@
+//lista_simples.h
+#include <iostream>
+using namespace std;
+struct lista{
+	int info;
+	struct lista* prox;
+};
+typedef struct lista Lista;
+
+//Demais cabeçalhos de funçao da biblioteca
+Lista* inicializa();
+Lista* insere_inicio (Lista* l, int i);
+Lista* insereFim(Lista* l, int i);
+void imprime (Lista* l);
+void mostra_final(Lista* l);
+void mostra_inicio(Lista* l);
+bool IsEmpty(Lista* l);
+int tamanho(Lista* l);
+Lista* retira (Lista* l, int v);
+Lista* retiraInicio (Lista* l);
+Lista* retiraFim (Lista* l);
+Lista* busca (Lista* l, int v);
+Lista* ocorrencias(Lista* l, int v);
+
